55eb35ba2b272bd2381e0af499d782e70e0b6c01
CHUIJO <joannechui@jCees-MacBook-Pro.local>
2016-03-13 19:11:18 -0400

    Call options page file
    enqueue options.php into functions.php

--- functions.php
+++ functions.php
@@ -150,3 +150,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Call Options page file.
+ */
+ require get_stylesheet_directory() .'/inc/options.php';
