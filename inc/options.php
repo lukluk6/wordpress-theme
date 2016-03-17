22392b85aec24a33e4ccce8a36a0ab4b0f1cfc8b
CHUIJO <joannechui@jCees-MacBook-Pro.local>
2016-03-17 00:11:54 -0400

    edit error

--- inc/options.php
+++ inc/options.php
@@ -1,4 +1,10 @@
 <?php
+
+function cd_add_submenu() {
+		add_submenu_page( 'themes.php', 'My Super Awesome Options Page', 'Theme Options', 'manage_options', 'theme_options', 'my_theme_options_page');
+	}
+add_action( 'admin_menu', 'cd_add_submenu' );
+
 // Upload a Logo
 	function logo_display()
 {
