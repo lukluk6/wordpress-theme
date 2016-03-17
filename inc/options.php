489e41390766885e4c769a849e66ac564f32a50a
CHUIJO <joannechui@jCees-MacBook-Pro.local>
2016-03-17 03:47:42 -0400

    change the google font code

--- inc/options.php
+++ inc/options.php
@@ -2,11 +2,18 @@
 	
 function cd_add_submenu() {
 		add_submenu_page( 'themes.php', 'Customize Yourself', 'Theme Options', 'manage_options', 'theme_options', 'my_theme_options_page');
+		//Activate custom settings
+		add_action( 'admin_init', 'cd_settings_init' );
+	
 	}
+	
+	
 add_action( 'admin_menu', 'cd_add_submenu' );
 	
 
 function cd_settings_init() { 
+
+register_setting( 'theme_options', 'cd_options_settings' );
 	
 	
 	add_settings_section(
@@ -31,14 +38,16 @@ function cd_settings_init() {
 		'cd_options_page_section' //section
 	);
 	
+	register_setting("cd_options_page_section", "cd_twitter_url");
+	
 	function display_twitter_element() { 
 		$options = get_option( 'cd_twitter_url' );
 		?>
-		<input type="text" name="cd_twitter_url" id="cd_twitter_url" value="<?php echo get_option('cd_twitter_url'); ?>" />
+		<input type="text" name="cd_twitter_url" id="cd_twitter_url" value="" <?php echo get_option('cd_twitter_url'); ?>" />
 		<?php
 	}
 	
-	register_setting("cd_options_page_section", "cd_twitter_url");
+	
 
 	//Choose between layouts
 	add_settings_field( 
@@ -105,11 +114,11 @@ function cd_settings_init() {
 	}
 	
 	function my_theme_options_page(){ 
-		?>
+	?>
 		<form action="options.php" method="post">
 			<h2>Customize Yourself</h2>
 			<?php
-			settings_fields( 'theme_options' );
+			settings_fields( 'cd_options_page_section' );
 			do_settings_sections( 'theme_options' );
 			submit_button();
 			?>
@@ -118,5 +127,3 @@ function cd_settings_init() {
 	}
 
 }
-
-add_action( 'admin_init', 'cd_settings_init' );
