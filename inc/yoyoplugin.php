74c642c4dff8d652f1f5e8c477ebcd5e688c7f8b
lukluk6 <jinglebell.lu@mail.utoronto.ca>
2016-03-23 20:12:24 -0400

    make it display in plugin page
    make the plugin visible on the plugin page

--- inc/yoyoplugin.php
+++ inc/yoyoplugin.php
@@ -2,8 +2,20 @@
 /*
 	Plugin Name: Yoyo Plugin
 	Plugin URI: http://http://phoenix.sheridanc.on.ca/~ccit3407/
-	Description: This is a plugin that display
+	Description: This is a plugin that display yoga post of the day.
 	Version:1.0
 	Author: Shirlet Chan, Joanne Chui, Jing Lu
 	Author URI:http://http://phoenix.sheridanc.on.ca/~ccit3407/
 */
+class YoyoPlugin extends WP_Widget {
+	public function __construct() {
+	$widget_ops = array(
+		'classname' => 'widget_archive',
+		'description' => __( 'a plugin show you yoga of the day', 'JJS')
+		);
+	parent::__construct('yoyo_plugin', __('Yoyo Plugin', 'JJS'), $widget_ops); 
+}
+}
+
+add_action( 'widgets_init', function(){ register_widget( 'YoyoPlugin' );
+}); //outside of the class
