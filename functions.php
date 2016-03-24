8db67903fabf93ef00b83bd0d9fcaa96b3040656
CHUIJO <joannechui@jCees-MacBook-Pro.local>
2016-03-24 03:11:42 -0400

    functions php

--- functions.php
+++ functions.php
@@ -185,8 +185,6 @@ function modify_read_more_link() {
 add_image_size ('home-slider', 960,280, true);
 
 
-<<<<<<< Updated upstream
-=======
 // Call the file that makes our new widget
 require get_stylesheet_directory() . '/inc/thewidget.php';
 >>>>>>> Stashed changes
