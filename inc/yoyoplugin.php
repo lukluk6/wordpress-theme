43c865b82ca1f878085782400760e924ae726531
shirletchan <shirlet.chan@mail.utoronto.ca>
2016-03-23 22:05:54 -0400

    testing "yoga of the day" widget

--- inc/yoyoplugin.php
+++ inc/yoyoplugin.php
@@ -29,26 +29,55 @@ class YoyoPlugin extends WP_Widget {
                  );
                  
           	echo $args['before_widget'];
-		if ( ! empty( $ctitle) )
-			echo $args['before_title'] . $ctitle. $args['after_title'];
+		if ( ! empty( $title) )
+			echo $args['before_title'] . $title. $args['after_title'];
 
-			$jsonxix = file_get_contents('http://dir.itslum.com/quotes/api/' . $qtype. '.php');
-			$objxix = json_decode($jsonxix);
-			echo '<p>' . str_replace(";", "<br/>", $objxix->quote) . '</p>';
-			echo '<i><a href="http://dir.itslum.com/quotes/topic/' . str_replace(" ", "+", $objxix->author) . '" target="_blank">__' . $objxix->author . '</a></i>';
+			$yoyo = file_get_contents('http://dir.itslum.com/quotes/api/' . $type. '.php');
+			$jjs = json_decode($yoyo);
+			echo '<p>' . str_replace(";", "<br/>", $jjs->quote) . '</p>';
+			echo '<i><a href="http://dir.itslum.com/quotes/topic/' . str_replace(" ", "+", $jjs->author) . '" target="_blank">__' . $jjs->author . '</a></i>';
 
 
 		//echo __( '<script type="text/javascript" src="http://dir.itslum.com/quotes/api/' . $qtype. '/index.php?h=off"></script>', 'text_domain' );
 		echo $args['after_widget'];       
-                 
-                 
-                 
-                 
-                 
+                                
 }
 
+		function form( $instance ) {
+		if ( isset( $instance[ 'type' ] ) ) {
+			$type = $instance[ 'type' ];
+		}
+		else {
+			$qype = "qotd";
+		}
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = "Yoga Pose of the day";
+		}
+		?>
+	         <p>
+			<label for="<?php echo $this->get_field_id( 'qtype' ); ?>">Select quotation type:</label> 
+			<select id="<?php echo $this->get_field_id( 'qtype' ); ?>" name="<?php echo $this->get_field_name( 'qtype' ); ?>" class="widefat" style="width:100%;">
+				<option value="quote" <?php if ( 'quote' == $qtype ) echo 'selected="selected"'; ?>>Yoga Pose of the Day</option>		
+			</select>
+
+			<label for="<?php echo $this->get_field_id( 'ctitle' ); ?>">Title:</label>
+			<input id="<?php echo $this->get_field_id( 'ctitle' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" class="widefat" style="width:100%;" type="text" value="<?php echo $title; ?>"/>
+
+		</p>
+		<?php 
+	}
+	
+	 function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['type'] = ( ! empty( $new_instance['type'] ) ) ? strip_tags( $new_instance['type'] ) : '';
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
 
 
-
+//register Yoyo plugin widget//
 add_action( 'widgets_init', function(){ register_widget( 'YoyoPlugin' );
 }); //outside of the class
