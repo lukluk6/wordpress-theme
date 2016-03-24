90f83adeb0eb1af460850290df51133ed5ea0d4f
shirletchan <shirlet.chan@mail.utoronto.ca>
2016-03-24 02:01:16 -0400

    new shortcode php

---
+++ inc/my-shortcode.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Plugin Name: Shortcodes for Yoyo
+* Plugin URI: http://phoenix.sheridanc.on.ca/~ccit3407/
+* Description: Shortcode to link
+* Author: Shirlet Chan, Joanne Chui, Jing Lu
+* Author URI: http://phoenix.sheridanc.on.ca/~ccit3407/
+* Version: 1.0
+*/
+
