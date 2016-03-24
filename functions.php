75943dda7d6a7f94dec1a0ccf343861a7f3681c7
CHUIJO <joannechui@jCees-MacBook-Pro.local>
2016-03-24 03:15:06 -0400

    edit error

--- functions.php
+++ functions.php
@@ -192,4 +192,4 @@ require get_stylesheet_directory() . '/inc/thewidget.php';
 require get_stylesheet_directory() . '/inc/Yoyo.php';
 
 
-include ('/yoyoshortcode/shortcode.php');
+require get_stylesheet_directory() .'/yoyoshortcode/shortcode.php';
